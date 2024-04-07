eac8d3d5329c82beea5462e85e68c0c3f157f268
saketsanadhya <saketsanadhya2003@gmail.com>
2024-04-07 23:48:11 +0530

    delete tags

--- frontend/src/components/Tag.jsx
+++ frontend/src/components/Tag.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { EditorContext } from "../pages/Editor.pages";
 
 function Tag({ tag }) {
+
+    let{blog,blog:{tags},setBlog}=useContext(EditorContext)
+    const handleTagDelete=()=>{
+        tags=tags.filter(t=>t!=tag)
+        setBlog({...blog,tags})
+    }
   return (
     <div
       className="relative p-2 mt-2 mr-2 px-5 bg-white rounded-full
@@ -12,6 +19,7 @@ function Tag({ tag }) {
       <button
         className="mt-[2px] rounded-full absolute right-3 top-1/2
     -translate-y-1/2"
+      onClick={handleTagDelete}
       >
         <i className="fi fi-br-cross text-sm pointer-events-none"></i>
       </button>
