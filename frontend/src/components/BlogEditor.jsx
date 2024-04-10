42baad72ebb6a850e7b9401aa63e4b16bf238495
saketsanadhya <saketsanadhya2003@gmail.com>
2024-04-10 20:34:13 +0530

    save-draft

--- frontend/src/components/BlogEditor.jsx
+++ frontend/src/components/BlogEditor.jsx
@@ -1,107 +1,174 @@
-import React, { useContext, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import logo from "../imgs/logo.png"
-import AnimationWrapper from '../common/page-animation'
-import defaultBanner from "../imgs/blog banner.png"
-import {toast,Toaster} from "react-hot-toast"
-import { EditorContext } from '../pages/Editor.pages'
-import EditorJS from "@editorjs/editorjs"
-import { Tools } from './Tools'
+import React, { useContext, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../imgs/logo.png";
+import AnimationWrapper from "../common/page-animation";
+import defaultBanner from "../imgs/blog banner.png";
+import { toast, Toaster } from "react-hot-toast";
+import { EditorContext } from "../pages/Editor.pages";
+import EditorJS from "@editorjs/editorjs";
+import { Tools } from "./Tools";
+import axios from "axios";
+import { UserContext } from "../App";
 function BlogEditor() {
+  let {
+    blog,
+    blog: { title, banner, content, tags, des },
+    setBlog,
+    textEditor,
+    setTextEditor,
+    setEditorState,
+  } = useContext(EditorContext);
+  let navigate = useNavigate();
+  let {
+    userAuth: { access_token },
+  } = useContext(UserContext);
+  useEffect(() => {
+    if (!textEditor.isReady) {
+      setTextEditor(
+        new EditorJS({
+          holderId: "textEditor",
+          data: content,
+          tools: Tools,
+          placeholder: "Let's write an awesome story",
+        })
+      );
+    }
+  }, []);
 
-    let {blog,blog:{title,banner,content,tags,des},setBlog,textEditor,setTextEditor,setEditorState}=useContext(EditorContext)
+  const handleBannerUpload = (e) => {
+    let img = e.target.files[0];
+    console.log(img);
+  };
 
-    useEffect(()=>{
-        setTextEditor(new EditorJS({
-            holderId:"textEditor",
-            data:content,
-            tools:Tools,
-            placeholder:"Let's write an awesome story"
-        }))
-    },[])
+  const handleTitleKeyDown = (e) => {
+    console.log(e);
+    if (e.keyCode == 13) {
+      e.preventDefault();
+    }
+  };
+  const handleTitleChange = (e) => {
+    let input = e.target;
+    input.style.height = "auto";
+    input.style.height = input.scrollHeight + "px";
+    setBlog({ ...blog, title: input.value });
+    console.log(title);
+  };
+  const handleError = (e) => {
+    let img = e.target;
+    img.src = defaultBanner;
+  };
+  const handlePublishEvent = () => {
+    // if(!banner.length){
+    //     return toast.error("Upload a blog banner to publish it")
+    // }
+    if (!title.length) {
+      return toast.error("Write blog title to publish it");
+    }
+    if (textEditor.isReady) {
+      textEditor
+        .save()
+        .then((data) => {
+          if (data.blocks.length) {
+            setBlog({ ...blog, content: data });
+            setEditorState("publish");
+          } else {
+            return toast.error("Write something in your blog to publish it");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+  const handleSaveDraft = (e) => {
+    if (e.target.className.includes("disable")) {
+      return;
+    }
+    if (!title.length) {
+      return toast.error("Write blog title before saving it as draft");
+    }
+    let loadingToast = toast.loading("Saving Draft....");
+    e.target.classList.add("disable");
 
-    const handleBannerUpload=(e)=>{
-        let img=e.target.files[0]
-        console.log(img);
+    if (textEditor.isReady) {
+      textEditor.save().then((content) => {
+        let blogObj = {
+            title,
+            banner,
+            des,
+            content,
+            tags,
+            draft: true,
+          };
+        axios
+          .post(import.meta.env.VITE_SERVER_DOMAIN + "/create-blog", blogObj, {
+            headers: {
+              Authorization: `Bearer ${access_token}`,
+            },
+          })
+          .then(() => {
+            e.target.classList.remove("disable");
+            toast.dismiss(loadingToast);
+            toast.success("Saved");
+            setTimeout(() => {
+              navigate("/");
+            }, 500);
+          })
+          .catch(({ response }) => {
+            e.target.classList.remove("disable");
+            toast.dismiss(loadingToast);
+            return toast.error(response.data.error);
+          });
+      });
     }
-
-    const handleTitleKeyDown=(e)=>{
-        console.log(e);
-        if(e.keyCode==13){
-            e.preventDefault()
-        }
-    }
-    const handleTitleChange=(e)=>{
-        let input=e.target
-        input.style.height='auto'
-        input.style.height=input.scrollHeight+"px"
-        setBlog({...blog, title: input.value})
-        console.log(title)
-    }
-    const handleError=(e)=>{
-        let img=e.target
-        img.src=defaultBanner
-    }
-    const handlePublishEvent=()=>{
-        // if(!banner.length){
-        //     return toast.error("Upload a blog banner to publish it")
-        // }
-        if(!title.length){
-            return toast.error("Write blog title to publish it")
-        }
-        if(textEditor.isReady){
-            textEditor.save().then(data=>{
-                if(data.blocks.length){
-                    setBlog({...blog,content:data})
-                    setEditorState("publish")
-                }else{
-                    return toast.error("Write something in your blog to publish it")
-                }
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-        }
-    }
+    
+  };
   return (
     <>
-         <nav className='navbar'>
+      <nav className="navbar">
         <Link to="/" className="flex-none w-10">
-            <img src={logo} />
+          <img src={logo} />
         </Link>
-        <p className='max-md:hidden text-black line-clamp-1 w-full'>
-            {
-                title.length ? title:"New Blog"
-            }
+        <p className="max-md:hidden text-black line-clamp-1 w-full">
+          {title.length ? title : "New Blog"}
         </p>
-        <div className='flex gap-4 ml-auto'>
-            <button className='btn-dark py-2' onClick={handlePublishEvent}>
-                Publish
-            </button>
-            <button className='btn-light py-2'>
-                Save Draft
-            </button>
+        <div className="flex gap-4 ml-auto">
+          <button className="btn-dark py-2" onClick={handlePublishEvent}>
+            Publish
+          </button>
+          <button className="btn-light py-2" onClick={handleSaveDraft}>Save Draft</button>
         </div>
-         </nav>
-         <Toaster/>
-         <AnimationWrapper>
-            <section>
-                <div className='mx-auto max-w-[900px] w-full'>
-
-                    <div className='relative aspect-video hover:opacity-80 bg-white border-4 border-grey'>
-                        <label htmlFor="uploadBanner">
-                            <img src={banner} className='z-20' onError={handleError}/>
-                            <input type="file" id='uploadBanner' accept='.png .jpg .jpeg' hidden onChange={handleBannerUpload}/>
-                        </label>
-                    </div>
-                    <textarea defaultValue={title} placeholder='Blog Title' className='text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40' onKeyDown={handleTitleKeyDown} onChange={handleTitleChange}></textarea>
-                    <hr className='w-full opacity-10 my-5'/>
-                    <div id='textEditor' className='font-gelasio'></div>
-                </div>
-            </section>
-         </AnimationWrapper>
+      </nav>
+      <Toaster />
+      <AnimationWrapper>
+        <section>
+          <div className="mx-auto max-w-[900px] w-full">
+            <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+              <label htmlFor="uploadBanner">
+                <img src={banner} className="z-20" onError={handleError} />
+                <input
+                  type="file"
+                  id="uploadBanner"
+                  accept=".png .jpg .jpeg"
+                  hidden
+                  onChange={handleBannerUpload}
+                />
+              </label>
+            </div>
+            <textarea
+              defaultValue={title}
+              placeholder="Blog Title"
+              className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
+            ></textarea>
+            <hr className="w-full opacity-10 my-5" />
+            <div id="textEditor" className="font-gelasio"></div>
+          </div>
+        </section>
+      </AnimationWrapper>
     </>
-  )
+  );
 }
 
-export default BlogEditor
+export default BlogEditor;
--- frontend/src/components/PublishForm.jsx
+++ frontend/src/components/PublishForm.jsx
@@ -3,16 +3,24 @@ import AnimationWrapper from "../common/page-animation";
 import { Toaster, toast } from "react-hot-toast";
 import { EditorContext } from "../pages/Editor.pages";
 import Tag from "./Tag";
+import axios from "axios"
+import { UserContext } from "../App";
+import {useNavigate} from "react-router-dom"
 function PublishForm() {
   let characterLimit = 200;
   let tagLimit = 10;
 
   let {
     blog,
-    blog: { banner, title, tags, des },
+    blog: { banner, title, tags, des, content },
     setEditorState,
     setBlog,
   } = useContext(EditorContext);
+
+  let {userAuth:{access_token}}=useContext(UserContext)
+
+  let navigate=useNavigate()
+
   const handleCloseEvent = () => {
     setEditorState("editor");
   };
@@ -47,6 +55,44 @@ function PublishForm() {
       e.target.value = "";
     }
   };  
+  const publishBlog=(e)=>{
+    if(e.target.className.includes("disable")){
+      return
+    }
+    if(!title.length) {
+      return toast.error("Write blog title before publishing")
+      }
+    if(!des.length || des.length > characterLimit){
+      return toast.error(`Write a description about your blog within
+      ${characterLimit} characters to publish`)
+    }
+    if(!tags.length) {
+      return toast.error("Enter at least 1 tag to help us rank your blog")
+    }
+    let loadingToast=toast.loading("Publishing....")
+    e.target.classList.add('disable')
+    let blogObj={
+      title,banner,des,content,tags,draft:false
+    }
+    axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/create-blog",blogObj,{
+      headers:{
+        'Authorization':`Bearer ${access_token}`
+      }
+    })
+    .then(()=>{
+      e.target.classList.remove('disable')
+      toast.dismiss(loadingToast)
+      toast.success("Published")
+      setTimeout(()=>{
+          navigate("/")
+      },500)
+    })
+    .catch(({response})=>{
+      e.target.classList.remove('disable')
+      toast.dismiss(loadingToast)
+      return toast.error(response.data.error)
+    })
+  }
   return (
     <AnimationWrapper>
       <section className="w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4">
@@ -107,7 +153,7 @@ function PublishForm() {
             })}
           </div>
           <p className="mt-1 mb-4 text-dark-grey text-right">{tagLimit-tags.length} Task left</p>
-           <button className="btn-dark px-8">Publish</button>
+           <button className="btn-dark px-8" onClick={publishBlog}>Publish</button>
         </div>
       </section>
     </AnimationWrapper>
