e4cca3bb05a57e5ff4e1ff82d1f1239f14ef4f89
saketsanadhya <saketsanadhya2003@gmail.com>
2024-04-14 16:17:43 +0530

    latest-blog fetch

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/InPageNavigation";
+import axios from "axios"
+import Loader from "../components/Loader";
 function HomePage() {
+  let[blogs,setBlog]=useState(null)
+  const fetchLatestBlogs=()=>{
+    axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+    .then(({data})=>{
+      setBlog(data.blogs)
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+  useEffect(()=>{
+    fetchLatestBlogs()
+  },[])
   return (
     <AnimationWrapper>
       <section className="h-cover flex justify-center gap-10">
           {/* latest blogs */}
           <div className="w-full">
               <InPageNavigation routes={["home","trending blogs"]} defaultHidden={["trending blogs"]}>
-                  <h1>Latest blogs here</h1>
+                  <>
+                    {
+                      blogs==null ? <Loader/>:
+                      <h1>Blogs are in state</h1>
+                    }
+                  </>
                   <h1>Trending blogs here</h1>
               </InPageNavigation>
           </div>
--- server/server.js
+++ server/server.js
@@ -170,6 +170,22 @@ server.post("/google-auth", async (req, res) => {
         return res.status(500).json({"error":"Failed to authenticate you with google.Try with some other google account"})
     })
 });
+
+server.get("/latest-blogs",(req,res)=>{
+  let maxLimit=5
+
+  Blog.find({draft:false})
+  .populate("author","personal_info.profile_img personal_info.username personal_info.fullname -_id" )
+  .sort({"publishedAt":-1})
+  .select("blog_id title des banner activity tags publishedAt -_id")
+  .limit(maxLimit)
+  .then(blogs=>{
+    return res.status(200).json({blogs})
+  })
+  .catch(err=>{
+    return res.status(500).json({error:err.message})
+  })
+})
 server.post("/create-blog",verifyJWT,(req,res)=>{
     let authorId=req.user
     let{title,des,banner,tags,content,draft}=req.body
@@ -180,9 +196,9 @@ server.post("/create-blog",verifyJWT,(req,res)=>{
       if(!des.length || des.length > 200){
         return res.status(403).json({ error: "You must provide blog description under 200 characters" });
       }
-      if(!banner.length){
-        return res.status(403).json({ error: "You must provide blog banner to publish it" });
-      }
+      // if(!banner.length){
+      //   return res.status(403).json({ error: "You must provide blog banner to publish it" });
+      // }
       if(!content.blocks.length){
         return res.status(403).json({ error: "There must be some blog content to publish it" });
       }
