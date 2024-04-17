e04c48ac39ce4d2d07af8988f59c9d8d1cbba7bc
saketsanadhya <saketsanadhya2003@gmail.com>
2024-04-17 23:19:51 +0530

    trending and tags page

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -6,17 +6,19 @@ import Loader from "../components/Loader";
 import BlogPostCard from "../components/BlogPostCard";
 import MinimalBlogPost from "../components/MinimalBlogPost";
 import {activeTabRef } from "../components/InPageNavigation";
+import NoDataMessage from "../components/NoDataMessage";
 
 function HomePage() {
   let [blogs, setBlog] = useState(null);
   let [trendingBlogs, setTrendingBlog] = useState(null);
   let[pageState,setPageState]=useState("home")
   let categories = [
-    "programming",
-    "hollywood",
-    "film making",
-    "social media",
-    "cooking",
+    "cricket",
+    "football",
+    "finance",
+    "hockey",
+    "Holiday",
+    "film"
   ];
   const fetchLatestBlogs = () => {
     axios
@@ -29,6 +31,17 @@ function HomePage() {
       });
   };
 
+  const fetchBlogsByCategory=()=>{
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs",{tag:pageState})
+      .then(({ data }) => {
+        setBlog(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } 
+  
   const fetchTrendingBlogs = () => {
     axios
       .get(import.meta.env.VITE_SERVER_DOMAIN + "/trending-blogs")
@@ -53,6 +66,9 @@ function HomePage() {
     if(pageState=="home"){
     fetchLatestBlogs();
     }
+    else{
+      fetchBlogsByCategory()
+    }
     if(!trendingBlogs){
       fetchTrendingBlogs();
     }
@@ -71,6 +87,7 @@ function HomePage() {
               {blogs == null ? (
                 <Loader />
               ) : (
+                blogs.length?
                 blogs.map((blog, i) => {
                   return (
                     <AnimationWrapper
@@ -84,11 +101,13 @@ function HomePage() {
                     </AnimationWrapper>
                   );
                 })
+                : <NoDataMessage message="No blogs published"/>
               )}
             </>
             {trendingBlogs == null ? (
               <Loader />
             ) : (
+              trendingBlogs.length?
               trendingBlogs.map((blog, i) => {
                 return (
                   <AnimationWrapper
@@ -99,11 +118,12 @@ function HomePage() {
                   </AnimationWrapper>
                 );
               })
+              : <NoDataMessage message="No trending blogs"/>
             )}
           </InPageNavigation>
         </div>
         {/* filters and trending blogs */}
-        <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l border-grey pl-8 pt-3 max:md:hidden">
+        <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden">
           <div className="flex flex-col gap-10">
             <div>
               <h1 className="font-medium text-xl mb-8">
@@ -128,6 +148,7 @@ function HomePage() {
             {trendingBlogs == null ? (
               <Loader />
             ) : (
+              trendingBlogs.length?
               trendingBlogs.map((blog, i) => {
                 return (
                   <AnimationWrapper
@@ -138,6 +159,7 @@ function HomePage() {
                   </AnimationWrapper>
                 );
               })
+              : <NoDataMessage message="No trending blogs"/>
             )}
           </div>
           </div>
--- server/server.js
+++ server/server.js
@@ -200,6 +200,24 @@ server.get("/trending-blogs",(req,res)=>{
     return res.status(500).json({error:err.message})
   })
 })
+
+server.post("/search-blogs",(req,res)=>{
+  let{tag}=req.body
+  let findQuery={tags:tag,draft:false}
+  let maxLimit=5
+  Blog.find(findQuery)
+  .populate("author","personal_info.profile_img personal_info.username personal_info.fullname -_id")
+  .sort({"publishedAt":-1})
+  .select("blog_id title des banner activity tags publishedAt -_id")
+  .limit(maxLimit)
+  .then(blogs=>{
+    return res.status(200).json({blogs})
+  })
+  .catch(err=>{
+    return res.status(500).json({error:err.message})
+  })
+})
+
 server.post("/create-blog",verifyJWT,(req,res)=>{
     let authorId=req.user
     let{title,des,banner,tags,content,draft}=req.body
