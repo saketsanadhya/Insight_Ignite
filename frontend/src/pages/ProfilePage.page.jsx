e3d71bfe95209f3b38aacdcc51e4ebb16d7378cb
saketsanadhya <saketsanadhya2003@gmail.com>
2024-04-24 20:39:18 +0530

    blogs of users

--- frontend/src/pages/ProfilePage.page.jsx
+++ frontend/src/pages/ProfilePage.page.jsx
@@ -5,6 +5,12 @@ import Loader from "../components/Loader";
 import AnimationWrapper from "../common/page-animation";
 import { UserContext } from "../App";
 import AboutUser from "../components/AboutUser";
+import { FilterPaginationData } from "../common/FilterPaginationData";
+import InPageNavigation from "../components/InPageNavigation";
+import BlogPostCard from "../components/BlogPostCard";
+import NoDataMessage from "../components/NoDataMessage";
+import LoadMoreDataBtn from "../components/LoadMoreDataBtn";
+import PageNotFound from "./404.page";
 
 export const profileDataStructure = {
   personal_info: {
@@ -24,8 +30,10 @@ export const profileDataStructure = {
 function ProfilePage() {
   let { id: profileId } = useParams();
   let [profile, setProfile] = useState(profileDataStructure);
+  let [loading, setLoading] = useState(true);
+  let [blogs, setBlogs] = useState(null);
+  let[profileLoaded,setProfieLoaded]=useState("")
 
-  let[loading,setLoading]=useState(true)
   let {
     personal_info: { fullname, username: profile_username, profile_img, bio },
     account_info: { total_posts, total_reads },
@@ -33,7 +41,9 @@ function ProfilePage() {
     joinedAt,
   } = profile;
 
-  let{userAuth:{username}}=useContext(UserContext)
+  let {
+    userAuth: { username },
+  } = useContext(UserContext);
 
   const fetchUserProfile = () => {
     axios
@@ -41,51 +51,133 @@ function ProfilePage() {
         username: profileId,
       })
       .then(({ data: user }) => {
-        setProfile(user);
-        setLoading(false)
+
+        if(user!=null){
+          setProfile(user);
+        } 
+        setProfieLoaded(profileId)
+        getBlogs({user_id:user._id})
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
-        setLoading(false)
-
+        setLoading(false);
       });
   };
 
-  const resetStates=()=>{
-    setProfile(profileDataStructure)
-    setLoading(true)
-  }
+  const resetStates = () => {
+    setProfile(profileDataStructure);
+    setProfieLoaded("")
+    setLoading(true);
+  };
+
+  const getBlogs = ({ page = 1, user_id }) => {
+    user_id = user_id == undefined ? blogs.user_id : user_id;
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs", {
+        author: user_id,
+        page,
+      })
+      .then(async ({ data }) => {
+        let formatedDate = await FilterPaginationData({
+          state: blogs,
+          data: data.blogs,
+          page,
+          countRoute: "/search-blogs-count",
+          data_to_send: { author: user_id },
+        });
+        formatedDate.user_id = user_id;
+        setBlogs(formatedDate);
+      });
+  };
 
   useEffect(() => {
-    resetStates()
-    fetchUserProfile();
-  }, [profileId]);
-  return( 
-        <AnimationWrapper>
-        {
-            loading ? <Loader/>:
-            <section className="h-cover md:flex flex-row-reverse items-start gap-5 min-[1100px]:gap-12">
-                <div className="flex flex-col max-md:items-center gap-5 min-w-[250px]">
-                    <img src={profile_img} className="w-48 h-48 bg-grey rounded-full md:w-32 md:h-32" />
-                    <h1 className="text-2x1 font-medium">@{profile_username}</h1>
-                    <p className="text-xl capitalize h-6">{fullname}</p>
-                    <p>{total_posts.toLocaleString()} Blogs-{total_reads.toLocaleString()} Reads</p>
-                    <div className="flex gap-4 mt-2">
-                      {
-                      profileId == username ?
-                      <Link to="/settings/edit-profile"
-                      className="btn-light rounded-md">EditProfile</Link>
-                      :""
-                      }
-                      </div>
-                      <AboutUser className="max-md:hidden" bio={bio} social_links={social_links} joinedAt={joinedAt}/>
-                </div>
 
-            </section>    
+    if(profileId!=profileLoaded){
+      setBlogs(null)
+    }
+    if(blogs==null){
+      resetStates();
+      fetchUserProfile();
+    }
 
-        }
-        </AnimationWrapper>
-    )
+  }, [profileId,blogs]);
+
+  return (
+    <AnimationWrapper>
+      {loading ? (
+        <Loader />
+      ) : 
+          profile_username.length ? 
+        <section className="h-cover md:flex flex-row-reverse items-start gap-5 min-[1100px]:gap-12">
+          <div className="flex flex-col max-md:items-center gap-5 min-w-[250px] md:w-[50%] md:pl-8 md:border-l border-grey md:sticky md:top-[100px] md:py-10">
+            <img
+              src={profile_img}
+              className="w-48 h-48 bg-grey rounded-full md:w-32 md:h-32"
+            />
+            <h1 className="text-2x1 font-medium">@{profile_username}</h1>
+            <p className="text-xl capitalize h-6">{fullname}</p>
+            <p>
+              {total_posts.toLocaleString()} Blogs-
+              {total_reads.toLocaleString()} Reads
+            </p>
+            <div className="flex gap-4 mt-2">
+              {profileId == username ? (
+                <Link
+                  to="/settings/edit-profile"
+                  className="btn-light rounded-md"
+                >
+                  EditProfile
+                </Link>
+              ) : (
+                ""
+              )}
+            </div>
+            <AboutUser
+              className="max-md:hidden"
+              bio={bio}
+              social_links={social_links}
+              joinedAt={joinedAt}
+            />
+          </div>
+            <div className="max-md:mt-12 w-full">
+            <InPageNavigation
+            routes={["Blogs Published", "About"]}
+            defaultHidden={["About"]}
+          >
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : blogs.results.length ? (
+                blogs.results.map((blog, i) => {
+                  return (
+                    <AnimationWrapper
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                      key={i}
+                    >
+                      <BlogPostCard
+                        content={blog}
+                        author={blog.author.personal_info}
+                      />
+                    </AnimationWrapper>
+                  );
+                })
+              ) : (
+                <NoDataMessage message="No blogs published" />
+              )}
+              <LoadMoreDataBtn
+                state={blogs}
+                fetchDataFun={getBlogs}
+              />
+            </>
+              <AboutUser bio={bio} social_links={social_links} joinedAt={joinedAt}/>
+          </InPageNavigation>
+            </div>
+        </section>
+        :<PageNotFound/>
+      }
+    </AnimationWrapper>
+  );
 }
 
 export default ProfilePage;
--- server/server.js
+++ server/server.js
@@ -214,13 +214,16 @@ server.get("/trending-blogs",(req,res)=>{
 })
 
 server.post("/search-blogs",(req,res)=>{
-  let{tag,query,page}=req.body
+  let{tag,query,author,page}=req.body
   let findQuery
   if(tag){
     findQuery={tags:tag,draft:false}
   }
   else if(query){
     findQuery={draft:false,title:new RegExp(query,'i')}
+  }
+  else if(author){
+    findQuery={author,draft:false}
   }
   
   let maxLimit=2
@@ -240,13 +243,16 @@ server.post("/search-blogs",(req,res)=>{
 })
 
 server.post("/search-blogs-count",(req,res)=>{
-  let{tag,query}=req.body
+  let{tag,author,query}=req.body
   let findQuery
   if(tag){
     findQuery={tags:tag,draft:false}
   }
   else if(query){
     findQuery={draft:false,title:new RegExp(query,'i')}
+  }
+  else if(author){
+    findQuery={author,draft:false}
   }
   Blog.countDocuments(findQuery)
   .then(count=>{
