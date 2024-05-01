f70550c727f95cd6483c53d7ef974c67b72e1596
saketsanadhya <saketsanadhya2003@gmail.com>
2024-05-01 15:42:09 +0530

    comments-database

--- frontend/src/components/CommentField.jsx
+++ frontend/src/components/CommentField.jsx
@@ -1,11 +1,50 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../App'
+import toast, { Toaster } from 'react-hot-toast'
+import { BlogContext } from '../pages/BlogPage.page'
+import axios from 'axios'
 
 function CommentField({action}) {
+
+    let{blog,blog:{_id,author:{_id:blog_author},comments,activity,activity:{total_comments,total_parent_comments}},setBlog,setTotalParentCommentsLoaded}=useContext(BlogContext)
+    let{userAuth:{access_token,username,fullname,profile_img}}=useContext(UserContext)
     const[comment,setComment]=useState("")
+
+    const handleComment=()=>{
+        if(!access_token){
+            toast.error("Login first to leave a comment")
+        }
+         if(!comment.length){
+            toast.error("Write something to leave a comment")
+        }
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/add-comment",{
+            _id,blog_author,comment
+        },{
+            headers:{
+                'Authorization':`Bearer ${access_token}`
+            }
+        })
+        .then(({data})=>{
+            setComment("")
+            data.commented_by={personal_info:{username,profile_img,fullname}}
+
+            let newCommentArr
+            data.childrenLevel=0
+            newCommentArr=[data]
+            let parentCommentIncrementVal=1
+            setBlog({...blog,comments:{...comments,results:newCommentArr},activity:{...activity,total_comments:total_comments+1, total_parent_comments:total_parent_comments + parentCommentIncrementVal}})
+
+            setTotalParentCommentsLoaded(preVal=>preVal+parentCommentIncrementVal)
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
   return (
     <>
+    <Toaster/>
         <textarea value={comment} onChange={(e)=>setComment(e.target.value)} placeholder='Leave a comment....' className='resize-none input-box pl-5 placeholder:text-dark-grey h-[150px] overflow-auto'></textarea>
-        <button className='btn-dark mt-5 px-10'>{action}</button>
+        <button className='btn-dark mt-5 px-10' onClick={handleComment}>{action}</button>
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ import { getAuth } from "firebase-admin/auth";
 import User from "./Schema/User.js";
 import Blog from "./Schema/Blog.js"
 import Notification from "./Schema/Notification.js"
+import Comment from "./Schema/Comment.js"
 
 const server = express();
 let PORT = 3000;
@@ -409,6 +410,67 @@ server.post("/isliked-by-user",verifyJWT,(req,res)=>{
     return res.status(500).json({error:err.message})
   })
 })
+
+server.post("/add-comment", verifyJWT, (req, res) => {
+  let user_id = req.user; 
+  let { _id, comment, blog_author } = req.body;
+  if (!comment.length) {
+    return res.status(403).json({ error: "Write something to leave a comment" });
+  }
+  let commentObj = new Comment({
+    blog_id: _id,
+    blog_author,
+    comment,
+    commented_by: user_id, 
+  });
+  commentObj.save().then((commentFile) => {
+    let { comment, commentedAt, children } = commentFile;
+
+    Blog.findOneAndUpdate(
+      { _id },
+      {
+        $push: { comments: commentFile._id },
+        $inc: { "activity.total_comments": 1 },
+        "activity.total_parent_comments": 1,
+      }
+    )
+      .then((blog) => {
+        console.log('New comment created');
+      })
+      .catch((err) => {
+        console.error('Error updating blog:', err);
+      });
+
+    let notificationObj = {
+      type: "comment",
+      blog: _id,
+      notification_for: blog_author,
+      user: user_id, 
+      comment: commentFile._id,
+    };
+
+    new Notification(notificationObj)
+      .save()
+      .then((notification) => {
+        console.log('New notification created');
+      })
+      .catch((err) => {
+        console.error('Error creating notification:', err);
+      });
+
+    return res.status(200).json({
+      comment,
+      commentedAt,
+      _id: commentFile._id,
+      user_id,
+      children,
+    });
+  });
+});
+
 server.listen(PORT, () => {
   console.log("listening on port " + PORT);
 });
+
+
+
